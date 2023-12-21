fe8bfacf17ed9bec6dc5bd160261c9bb1a650a81
Hla <hla.thuzar@aleph-labs.com>
2023-12-21 11:03:16 +0800

    add testNG Data provider

---
+++ src/test/java/SeleniumPractise/TestNGDataProvider.java
@@ -0,0 +1,40 @@
+package SeleniumPractise;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TestNGDataProvider {
+
+    WebDriver driver;
+    @BeforeTest
+    public  void setUp()
+    {
+        System.setProperty("webdriver.chrome.driver","/Users/fahiezah/Desktop/2024Projects/AmazonPractise/chromedriver" );
+        driver = new ChromeDriver();
+    }
+
+    @Test(dataProvider = "dataSupport") // here mention dataProvider name
+    public void verifyLogin(String username, String password)
+    {
+        System.out.println("Username is : "+username + " and " +"Password is : "+ password);
+    }
+
+    @DataProvider(name = "dataSupport") //can add any name to DataProvider attribute
+    public Object[][] dataSuplply()
+    {
+        Object[][] data = {{"testdata1","1234"},{"testdata3","4567"},{"testdata1","8920"}};
+        return data;
+    }
+
+
+   @AfterTest
+   public void tearDown()
+   {
+       driver.quit();
+   }
+
+}
